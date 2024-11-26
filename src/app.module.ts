308dffd2fb175dbaec0ba99ec374a8dbababa159
wymsa <wymsamail@gmail.com>
2024-11-26 14:50:50 +0200

    feat: added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { defaultEnvConfig } from './common/configurations/env/env.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { defaultDatabaseConfig } from './common/configurations/database/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(defaultEnvConfig)],
+  imports: [
+    ConfigModule.forRoot(defaultEnvConfig),
+    TypeOrmModule.forRootAsync(defaultDatabaseConfig),
+  ],
   controllers: [],
   providers: [],
 })
