3041102def6b3889a0d0e4d14148e149133a5d0d
Anton Poliaiev <wymsa.email@gmail.com>
2025-03-15 16:59:29 +0200

    feat: added prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './common/prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [],
   providers: []
 })
