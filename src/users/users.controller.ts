0cd8a828fa2ee0eeffc2fa6e3f9d977ae072924e
Anton Poliaiev <wymsa.email@gmail.com>
2025-03-23 17:40:43 +0200

    feat: added user resource

---
+++ src/users/users.controller.ts
@@ -0,0 +1,62 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UpdateUserDTO, UserDTO } from './dtos/user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createUserDto: UserDTO) {
+    return await this.usersService.create(createUserDto);
+  }
+
+  @Patch(':userID')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Param('userID', ParseIntPipe) userID: number,
+    @Body() updateUserDto: UpdateUserDTO
+  ) {
+    return await this.usersService.update(userID, updateUserDto);
+  }
+
+  @Delete(':userID')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('userID', ParseIntPipe) userID: number) {
+    return await this.usersService.delete(userID);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getAll(
+    @Query('take', new ParseIntPipe({ optional: true })) take?: number,
+    @Query('skip', new ParseIntPipe({ optional: true })) skip?: number
+  ) {
+    return await this.usersService.getAll(take, skip);
+  }
+
+  @Get('id/:userID')
+  @HttpCode(HttpStatus.OK)
+  async getOneByID(@Param('userID', ParseIntPipe) userID: number) {
+    return await this.usersService.getOneByID(userID);
+  }
+
+  @Get('email/:userEmail')
+  @HttpCode(HttpStatus.OK)
+  async getOneByEmail(@Param('userEmail') userEmail: string) {
+    return await this.usersService.getOneByEmail(userEmail);
+  }
+}
