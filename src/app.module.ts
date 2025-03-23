510fbae79313f876134ef416e2bac210a726fd65
Anton Poliaiev <wymsa.email@gmail.com>
2025-03-23 17:40:43 +0200

    feat: added user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './common/prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    PrismaModule
+  ],
   controllers: [],
   providers: []
 })
