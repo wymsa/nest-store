e639eb31f001aab7062e6a002bc5d502ce9c0875
Anton Poliaiev <wymsa.email@gmail.com>
2025-03-28 12:42:45 +0200

    feat: added unique constraint exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { UniqueConstraintExceptionFilter } from './common/filters/unique-constraint-exception/unique-constraint-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: false })
   );
+  app.useGlobalFilters(new UniqueConstraintExceptionFilter());
   await app.listen(configService.getOrThrow<number>('PORT'));
 }
 bootstrap();
