d0d4318f54f0136f62f7055e1425e779b800d7d1
Anton Poliaiev <wymsa.email@gmail.com>
2025-04-04 16:34:38 +0300

    feat: added category selector

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 import { CategoryDTO, UpdateCategoryDTO } from './dtos/category.dto';
 import slug from 'slug';
+import { defaultCategorySelector } from './selectors';
 
 @Injectable()
 export class CategoriesService {
@@ -10,7 +11,10 @@ export class CategoriesService {
   async create(createCategoryDto: CategoryDTO) {
     const categorySlug = slug(createCategoryDto.name, '-');
 
-    return this.prismaService.category.create({ data: { ...createCategoryDto, slug: categorySlug } });
+    return this.prismaService.category.create({
+      data: { ...createCategoryDto, slug: categorySlug },
+      select: defaultCategorySelector
+    });
   }
 
   async update(categoryID: number, updateCategoryDto: UpdateCategoryDTO) {
@@ -22,23 +26,30 @@ export class CategoriesService {
 
     return this.prismaService.category.update({
       where: { id: categoryID },
-      data: { ...updateCategoryDto, slug: categorySlug }
+      data: { ...updateCategoryDto, slug: categorySlug },
+      select: defaultCategorySelector
     });
   }
 
   async delete(categoryID: number) {
-    return this.prismaService.category.delete({ where: { id: categoryID } });
+    return this.prismaService.category.delete({ where: { id: categoryID }, select: defaultCategorySelector });
   }
 
   async getAll() {
-    return this.prismaService.category.findMany();
+    return this.prismaService.category.findMany({ select: defaultCategorySelector });
   }
 
   async getOneByID(categoryID: number) {
-    return this.prismaService.category.findUnique({ where: { id: categoryID } });
+    return this.prismaService.category.findUnique({
+      where: { id: categoryID },
+      select: defaultCategorySelector
+    });
   }
 
   async getOneBySlug(categorySlug: string) {
-    return this.prismaService.category.findUnique({ where: { slug: categorySlug } });
+    return this.prismaService.category.findUnique({
+      where: { slug: categorySlug },
+      select: defaultCategorySelector
+    });
   }
 }
