92985192029611f3ecc5ecbbc32fabf26ecbd01a
Anton Poliaiev <wymsa.email@gmail.com>
2025-04-04 15:42:04 +0300

    feat: added category model

---
+++ prisma/migrations/20250404124013_added_category_model/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
