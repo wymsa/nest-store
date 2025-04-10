64cb5a58ecd6018579264615c8078bf1a576932a
Anton Poliaiev <wymsa.email@gmail.com>
2025-04-10 15:55:11 +0300

    refactor: updated and cleaned storage resource

--- src/common/storage/storage.module.ts
+++ src/common/storage/storage.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StorageService } from './storage.service';
-import { StorageController } from './storage.controller';
 import { S3StorageStrategy } from './strategies/s3-storage.strategy';
 import { IStorageStrategyToken } from './strategies/storage.strategy';
 
 @Module({
-  controllers: [StorageController],
   providers: [
     {
       provide: IStorageStrategyToken,
